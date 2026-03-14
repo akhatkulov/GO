d295353ee4806f7897b5ffd4457828eb8242898b
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2026-03-14 14:59:19 +0500

    Update main.go

--- TCPProxy/main.go
+++ TCPProxy/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net"
+	"log"
 )
 
+
 func main() {
-	fmt.Println("Salom")
+	quloq,err := net.Listen("tcp",":9999")
+	fmt.Println("TCPProxy ishladi!!!")
+	if err !=nil{
+		log.Fatal(err)
+	}
+	defer quloq.Close()
+
+	for {
+		conn,err := quloq.Accept()
+		if err!=nil{
+			log.Fatal(err)
+		}
+		
+	}
 }
